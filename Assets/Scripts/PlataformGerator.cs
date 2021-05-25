bbb2a17953417da97799c19bc2411858db17a49f
Davi Costa <daviita1@hotmail.com>
2021-05-25 19:31:02 -0300

    Update PlataformGerator.cs

--- Assets/Scripts/PlataformGerator.cs
+++ Assets/Scripts/PlataformGerator.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlataformGerator : MonoBehaviour
 {
+    public int minRangeY = -8;
+    public int maxRangeY = 8;
+
     public GameObject thePlataform;
     public Transform generationPoint;
-    public float distanceBtween;
+    public float distanceBtween = 25;
 
     private float plataformWidth;
+
+    private float distanceBtweenMin;
+    private float distanceBtweenMax;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +26,9 @@ public class PlataformGerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < generationPoint.position.x)
+        distanceBtween = Random.Range(distanceBtweenMin, distanceBtweenMax);
+
+        if (transform.position.x < generationPoint.position.x)
         {
             transform.position = new Vector2(transform.position.x + plataformWidth + distanceBtween, transform.position.y);
             Instantiate(thePlataform, transform.position, transform.rotation);
