b5fa7ff0d95ddbf562a8254bc056e9d8c49126e0
Davi Costa <daviita1@hotmail.com>
2021-05-27 12:50:38 -0300

    V3 com Parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -13,14 +13,14 @@ public class Parallax : MonoBehaviour
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float temp = (cam.transform.position.x*(1-parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1-parallaxEffect));
+        float distance = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y);
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
 
-        if (temp > startpos - length) startpos -= length;
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
